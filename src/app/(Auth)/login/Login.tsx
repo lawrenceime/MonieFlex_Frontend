9ef234c7192f521476e028cbac67f0648a151c5b
Lawrence Ime <edetime94@gmail.com>
2024-07-17 11:43:43 +0100

    Finalized working on the background image for the login page

--- src/app/(Auth)/login/Login.tsx
+++ src/app/(Auth)/login/Login.tsx
@@ -2,17 +2,14 @@ import React from 'react'
 import Image from 'next/image'
 import LoginCard from '@/components/Auth/LoginCard'
 
-
 const Login = () => {
     return (
         <div className='flex w-screen h-screen'>
             <LoginCard />
-            <div className='relative flex-1'>
-
-                <Image src='/assets/Abstract 3d design elements arrangement.svg' alt='Login Image' layout='fill' className='object-cover' />
+            <div className='relative flex-1 bg-cover bg-center' style={{ backgroundImage: "url('/assets/Abstract 3d design elements arrangement.svg')" }}>
             </div>
         </div>
-    )
+    );
 }
 
 export default Login
